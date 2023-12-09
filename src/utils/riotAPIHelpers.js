b363b906a2ce1102f71c8d92daedaba14560832b
Tae's PC <53635845+taeyeong2k@users.noreply.github.com>
2023-12-09 17:08:27 +1100

    fix imports

--- src/utils/riotAPIHelpers.js
+++ src/utils/riotAPIHelpers.js
@@ -1,4 +1,4 @@
-const { RiotAPI, RiotAPITypes, PlatformId } = require('@fightmegg/riot-api');
+const { RiotAPI } = require('@fightmegg/riot-api');
 
 const rAPI = new RiotAPI(process.env.RIOT_API_KEY);
 
