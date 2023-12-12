67b417853f3ad2b685023a57d5739cf32736e65f
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-13 09:28:48 +1100

    update test

--- src/tests/basic.js
+++ src/tests/basic.js
@@ -1,9 +1,6 @@
-const path = require('path');
-require('dotenv').config();
 const { getLatestVersion, getChampionByName, getAllChampions, getChampionById } = require('../utils/datadragonHelper');
 
 async function main() {
-    const champions = await getAllChampions();
     const rakan = await getChampionById(497);
     console.log(rakan);
 }
