0f6f741aaac910c56350de54ce79d8987e8500d5
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-12 11:54:38 +1100

    add latestVersion function for ddragon

--- src/utils/datadragonHelper.js
+++ src/utils/datadragonHelper.js
@@ -2,3 +2,14 @@ const { RiotAPI } = require('@fightmegg/riot-api');
 
 const rAPI = new RiotAPI(process.env.RIOT_API_KEY);
 
+async function getLatestVersion () {
+    try {
+        const version = await rAPI.DDragon.versions();
+        return version;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to fetch latest version.');
+    }
+}
+
+module.exports = { getLatestVersion };
